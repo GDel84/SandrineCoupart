54c30ce10ccd8e67aa8f49e4eef05cb74ebfd75e
GDel84 <g-del@hotmail.fr>
2022-09-27 20:53:20 +0200

    user methode

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -2,17 +2,62 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\UserFormType;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     #[Route('/admin/user', name: 'admin_user')]
-    public function index(): Response
+    public function index(UserRepository $userRepo): Response
     {
         return $this->render('/admin/user/admin-user.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $userRepo->findAll(),
         ]);
     }
+    #[Route('/admin/user/create/{id}', name: 'admin_user_create')]
+        public function ajoutUser(ManagerRegistry $doctrine, Request $request)
+        {
+            $user = new User;
+    
+            $form = $this->createForm(UserFormType::class, $user);
+            $form->handleRequest($request);
+    
+            if($form->isSubmitted() && $form->isValid()){
+    
+                    $em = $doctrine->getManager();
+                    $em->persist($user);
+                    $em->flush();
+            }
+            return $this->render('/admin/user/admin-user-create.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
+    
+        #[Route('/admin/user/edit/{id}', name: 'admin_user_edit')]
+        public function ModifierUser(ManagerRegistry $doctrine, $id, Request $request)
+        {
+            $userRepo = $doctrine->getRepository(User::class);
+            $user = $userRepo->findOneBy(['id'=>$id]);
+            $form = $this->createForm(UserFormType::class, $user);
+    
+            $form->handleRequest($request);
+    
+            if($form->isSubmitted() && $form->isValid()){
+                $em = $doctrine->getManager();
+                $em->persist($user);
+                $em->flush();
+    
+                return $this->redirectToRoute('/admin/user');
+            }
+    
+            return $this->render('/admin/user/admin-user-edit.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
 }
--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -5,6 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,8 +20,16 @@ class UserFormType extends AbstractType
             ->add('Nom')
             ->add('Prenom')
             ->add('telephone')
-            ->add('email')
-            ->add('roles')
+            ->add('email', EmailType::class)
+            ->add('roles', CollectionType::class,[
+                'entry_type'   => ChoiceType::class,
+                'entry_options'  => [
+                    'choices' => [
+                        'Admin' =>"ROLE_ADMIN",
+                        'User' =>"ROLE_USER",
+                    ]
+                ]
+            ])
             ->add('password')
             ->add('IdRegime')
             ->add('IdIngredient')
