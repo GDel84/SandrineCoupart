dbe191faa5c3614e27548bfc06659e6232daac09
GDel84 <g-del@hotmail.fr>
2022-09-28 17:04:03 +0200

    Add Methode etape, Recette-ingredient

--- src/Controller/Admin/RecetteController.php
+++ src/Controller/Admin/RecetteController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Etape;
+use App\Entity\Ingredient;
+use App\Entity\IngredientRecette;
 use App\Entity\Recette;
+use App\Form\EtapeFormType;
+use App\Form\IngredientRecetteFormType;
 use App\Form\RecetteFormType;
 use App\Repository\RecetteRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -115,9 +120,92 @@ class RecetteController extends AbstractController
 
             return $this->redirectToRoute('/admin/recette');
         }
-
         return $this->render('/admin/recette/admin-recette-edit.html.twig', [
             'form' => $form->createView()
         ]);
     }
+
+    #[Route('/admin/recette/ingredient/create/{idrecette}', name: 'admin_recette_ingredient_create')]
+        public function ajoutRecetteIngredient(ManagerRegistry $doctrine, Request $request)
+        {
+            $ingredientRecette = new IngredientRecette;
+    
+            $form = $this->createForm(IngredientRecetteFormType::class, $ingredientRecette);
+    
+            $form->handleRequest($request);
+    
+            if($form->isSubmitted() && $form->isValid()){
+    
+                    $em = $doctrine->getManager();
+                    $em->persist($ingredientRecette);
+                    $em->flush();
+            }
+            return $this->render('/admin/recette/ingredient/admin-recette-ingredient-create.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
+    
+        #[Route('/admin/recette/ingredient/edit/{id}', name: 'admin_recette_ingredient_edit')]
+        public function ModifierRecetteIngredient(ManagerRegistry $doctrine, $id, Request $request)
+        {
+            $ingredientRecetteRepo = $doctrine->getRepository(IngredientRecette::class);
+            $ingredientRecette = $ingredientRecetteRepo->findOneBy(['id'=>$id]);
+            $form = $this->createForm(IngredientRecetteFormType::class, $ingredientRecette);
+    
+            $form->handleRequest($request);
+    
+            if($form->isSubmitted() && $form->isValid()){
+                $em = $doctrine->getManager();
+                $em->persist($ingredientRecette);
+                $em->flush();
+    
+                return $this->redirectToRoute('/admin/ingredient');
+            }
+    
+            return $this->render('/admin/recette/ingredient/admin-recette-ingredient-edit.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
+
+        #[Route('/admin/recette/etape/create/{idrecette}', name: 'admin_recette_etape_create')]
+        public function ajoutEtape(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger)
+        {
+            $etape = new Etape;
+    
+            $form = $this->createForm(EtapeFormType::class, $etape);
+    
+            $form->handleRequest($request);
+    
+            if($form->isSubmitted() && $form->isValid()){
+    
+                    $em = $doctrine->getManager();
+                    $em->persist($etape);
+                    $em->flush();
+            }
+            return $this->render('/admin/recette/etape/Admin-recette-etape-create.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
+    
+        #[Route('/admin/recette/etape/edit/{id}', name: 'admin_recette_etape_edit')]
+        public function ModifierEtape(ManagerRegistry $doctrine, $id, Request $request)
+        {
+            $etapeRepo = $doctrine->getRepository(ingredient::class);
+            $etape = $etapeRepo->findOneBy(['id'=>$id]);
+            $form = $this->createForm(EtapeFormType::class, $etape);
+    
+            $form->handleRequest($request);
+    
+            if($form->isSubmitted() && $form->isValid()){
+                $em = $doctrine->getManager();
+                $em->persist($etape);
+                $em->flush();
+    
+                return $this->redirectToRoute('/admin/ingredient');
+            }
+    
+            return $this->render('/admin/recette/etape/Admin-recette-etape-edit.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
 }
